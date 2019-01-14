be1584f26223fcfa79a870b9a9e24815e0c51ff7
Valdir de Camargo <valdir.camargo@yahoo.com.br>
2019-01-14 22:40:48 +0000

    first commit Test commit

--- app/src/main/java/valdircamargo/com/br/ubp/MainActivity.java
+++ app/src/main/java/valdircamargo/com/br/ubp/MainActivity.java
@@ -1,13 +1,19 @@
 package valdircamargo.com.br.ubp;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,6 +25,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 public class MainActivity extends AppCompatActivity {
 
     Button btnSignUp, btnRegister;
+    RelativeLayout rootLayout;
 
     FirebaseAuth auth;
     FirebaseDatabase db;
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
         //Init View
         btnRegister =(Button)findViewById(R.id.btnRegister);
         btnSignUp = (Button)findViewById(R.id.btnSignIn);
+        rootLayout= (RelativeLayout) findViewById(R.id.rootLayout);
 
         //Event
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -59,21 +67,77 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showRegisterDialog() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("REGISTER ");
         dialog.setMessage("Please use email to resgister");
 
         LayoutInflater inflater = LayoutInflater.from(this);
         View register_layout = inflater.inflate(R.layout.layout_register,null);
 
-        MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
-        MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
-        MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
-        MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
 
         dialog.setView(register_layout);
 
         //Set Button
-        // hi testing commit
+        dialog.setPositiveButton("REGISTER", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+
+                //Check validation
+                if(TextUtils.isEmpty(edtEmail.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please enter email address",Snackbar.LENGTH_SHORT)
+                    .show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(edtPhone.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please enter your Phone Number",Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(edtName.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please enter your Name ",Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(edtPassword.getText().toString()))
+                {
+                    Snackbar.make(rootLayout,"Please enter your Password",Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                if((edtPassword.getText().toString().length() < 6 ))
+                {
+                    Snackbar.make(rootLayout,"Password too short !!!",Snackbar.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                //Save users to db
+
+                            }
+                        });
+            }
+        });
+        dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
     }
 }
---
+++ app/src/main/java/valdircamargo/com/br/ubp/Model/User.java
@@ -0,0 +1,20 @@
+package valdircamargo.com.br.ubp.Model;
+
+/**
+ * Created by Valdir on 13/01/2019.
+ */
+
+public class User {
+    private String email,password,name,phone;
+
+    public User() {
+
+    }
+
+    public User(String email, String password, String name, String phone) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+    }
+}
